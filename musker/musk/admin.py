31ded5492512631cc2a3dae3f5f44af159b20424
Kapil <2909kapil2001@gmail.com>
2025-07-25 16:05:25 +0530

    made profile pages

--- musker/musk/admin.py
+++ musker/musk/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group,User
-
+from .models import Profile
 
 # Register your models here.
 admin.site.unregister(Group)
+
+# mixing profile info into user info
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
 
 
 class UserAdmin(admin.ModelAdmin):
     model = User
     fields = ["username"]
+    inlines = [ProfileInline]
 
 
 admin.site.unregister(User)
 admin.site.register(User,UserAdmin)
+# admin.site.register(Profile)
--- musker/musker/settings.py
+++ musker/musker/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'musker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,9 +114,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
